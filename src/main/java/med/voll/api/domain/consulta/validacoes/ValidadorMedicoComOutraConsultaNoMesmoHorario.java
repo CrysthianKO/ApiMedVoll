6cddeb4e68dffc1598c879242b05eefdd8c2b304
CrysthianKO <crysthiansant013@gmail.com>
2023-12-22 12:42:10 -0300

    Adicionando regras de negocios

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamendoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorMedicoComOutraConsultaNoMesmoHorario implements ValidadorAgendamentoDeConsulta {
+
+    @Autowired
+    private ConsultaRepository repository;
+
+    public void validar(DadosAgendamendoConsulta dados) {
+         var medicoPossuiOutraConsultaNoMesmoHorario = repository.existsByMedicoAndData(dados.idMedico(), dados.data());
+         if (medicoPossuiOutraConsultaNoMesmoHorario) {
+             throw new ValidacaoException("Médico já possui outra consulta agendada nesse mesmo horário");
+         }
+    }
+}
